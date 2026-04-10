ed7f0dbdd62fdb570533094f6095f625cd616357
Kiprono <theyhavescored4@gmail.com>
2026-04-10 16:23:48 +0300

    update api

--- dragonbones/batch_models/BatchDragonModel.go
+++ dragonbones/batch_models/BatchDragonModel.go
@@ -1,6 +1,11 @@
 components {
   id: "BatchDragonModel"
   component: "/dragonbones/batch_models/BatchDragonModel.script"
+  properties {
+    id: "u_texture"
+    value: "/builtins/graphics/particle_blob.png"
+    type: PROPERTY_TYPE_HASH
+  }
 }
 embedded_components {
   id: "factory"
