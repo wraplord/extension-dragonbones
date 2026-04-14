0f46fa3ead014d5e459b8403c3bde70b161cd426
Kiprono <theyhavescored4@gmail.com>
2026-04-14 21:48:59 +0300

    Multiple characters sample in main

--- dragonbones/shared/instance.lua
+++ dragonbones/shared/instance.lua
@@ -1,3 +1,14 @@
 local M = {}
 M.instances = {}
+
+function M.create_buffer_prefix_name()
+	local tbl = {}
+	local alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+	for i = 1, 20 do
+		local pos = math.random(1, string.len(alphabet))
+		table.insert(tbl, string.sub(alphabet, pos, pos))
+	end
+	return table.concat(tbl, "")
+end
+
 return M
